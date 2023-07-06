d74998d7369a8b7cd2968cb704bf858a4f975a42
Naga564 <yanagayakisoba@gmail.com>
2023-07-06 14:48:21 +0900

    add 319 simulation

--- js/main.js
+++ js/main.js
@@ -1,16 +1,35 @@
 var result = 0;
 
-var number = 0;
-var count = 0;
+var number = 0; 
+var count = 0; //回転数
+var hitcount = 0; //初当たり回数
+var totalcount = 0; //総回転数
 
-while(1){
-    number = Math.random()*65535;
+var start = document.getElementById("start");
+start.addEventListener("click",hitcounter)
 
-    if(number <= 206){
-        break;
-    }else{
-        count++;
+
+function hitcounter(){
+    count = 1;
+    while(1){
+        number = Math.random()*65535;
+    
+        // 206/65535 = 1/319
+        if(number <= 206){
+            //初当たり
+            hitcount++;
+            totalcount += count;
+            break;
+        }else{
+            //はずれ
+            count++;
+        }
     }
+
+    //結果表示
+    var result = document.getElementById("result");
+    result.innerHTML = "回転数:"+count+"回<br>総当たり回数:"+hitcount+"回<br>総回転数:"+totalcount+"回";
+    
+    console.log(count);
 }
 
-print(count);
